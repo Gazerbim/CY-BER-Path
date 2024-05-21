e82aebc96ac0252909188a6c336ca29459ede2c9
tom200504 <166423945+tom200504@users.noreply.github.com>
2024-05-21 21:55:09 +0200

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -8,6 +8,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define RED_TEXT "\x1b[31m"
+#define RESET_TEXT "\x1b[0m"
+
 
 typedef struct{  // definition of a table cell
     int wall;
@@ -15,6 +18,7 @@ typedef struct{  // definition of a table cell
     int robot;
 }Cell;
 
+void printMapRed(Cell ** map, int x, int y,int cible);
 void printMap(Cell ** map, int x, int y);
 void addBorderWalls(Cell **map, int x,int y);
 Cell ** generateMap(int *x, int *y);
@@ -25,8 +29,8 @@ void placeWallsOnAnEdge(Cell ** map, int x, int y);
 void placeRobots(Cell ** map, int x, int y);
 int checkMove(Cell** map, char dir, int Posx, int Posy );
 int findRobot(Cell ** map, int *x, int *y, int widthMap,int lenghtMap, int numrobot);
-int moveInDirection(Cell** map, int lenghtMap,int widthMap, int targ,int numRobot );
-int abandon(int a);
+int moveInDirection(Cell** map, int lenghtMap,int widthMap, int targ,int numRobot);
+int abandon(char a);
 int moveTurn(Cell ** map, int targ, int lenghtMap, int widthMap, int numRobot );
 int chrono(int a);
 int harder();
