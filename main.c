9d096525633cff26c8e24ab4401d237b2bf70287
Gazerbim <100291331+Gazerbim@users.noreply.github.com>
2024-05-22 14:35:40 +0200

    added the "========================"

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(){
 		else{
 			verification=0;
 			while(getchar() != '\n'); //flushes the input buffer until a newline character ('\n') is encountered
-                    printf("\033[H\033[2JBIENVENUE DANS CY-BERPATH!\n\n\n");
+                    printf("\033[H\033[2J===========BIENVENUE DANS CY-BERPATH!===========\n\n\n");
             		printf("! Veuillez saisir un nombre valide supérieur à 0. ! \n");
 		}
 	}while(verification!=1);
@@ -22,12 +22,12 @@ int main(){
 	char prenom[100];
 	char **names=malloc(nb_players*sizeof(char*)); //malloc of the names array
 	if(names==NULL){
-        printf("\033[H\033[2JBIENVENUE DANS CY-BERPATH!\n\n\n");
+        printf("\033[H\033[2J===========BIENVENUE DANS CY-BERPATH!===========\n\n\n");
 		printf("L'allocation du tableau a planté, le programme va se fermer.\n");
 		exit(1);
 	}
 	for(int i=0;i<nb_players;i++){
-	    printf("\033[H\033[2JBIENVENUE DANS CY-BERPATH!\n\n\n");
+	    printf("\033[H\033[2J===========BIENVENUE DANS CY-BERPATH!===========\n\n\n");
 		printf("Saisir le pseudo du %deme joueur :",i+1);
 		scanf("%s",prenom);
 		nmb_letter=strlen(prenom);
@@ -46,7 +46,7 @@ int main(){
 
 	for(int i=0;i<nb_players;i++);
 	do{
-        printf("\033[H\033[2JBIENVENUE DANS CY-BERPATH!\n\n\n");
+        printf("\033[H\033[2J===========BIENVENUE DANS CY-BERPATH!===========\n\n\n");
 		printf("Combien de manche(s) voulez vous faire ?:");
 		scanf("%d",&nb_games);
 		if(nb_games>0){
@@ -55,13 +55,13 @@ int main(){
 		else{
 			verification=0;
 			while(getchar() != '\n'); //flushes the input buffer until a newline character ('\n') is encountered
-                    printf("\033[H\033[2JBIENVENUE DANS CY-BERPATH!\n\n\n");
+                    printf("\033[H\033[2J===========BIENVENUE DANS CY-BERPATH!===========\n\n\n");
             		printf(" ! Veuillez saisir un nombre valide superieur à 0. !\n");
 		}
 	}while(verification!=1);
 	int *tabScore=malloc(nb_players*sizeof(int)); //to store the scores of the players
 	if(tabScore==NULL){
-        printf("\033[H\033[2JBIENVENUE DANS CY-BERPATH!\n\n\n");
+        printf("\033[H\033[2J===========BIENVENUE DANS CY-BERPATH!===========\n\n\n");
 		printf("L'alloc n'a pas réussi, le programme va se fermer.\n");
 		exit(5);
 	}
